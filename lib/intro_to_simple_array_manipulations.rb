168f5d6eae6ad01a33ce20e4a3b3ad994d7b3ece
Alex Bonwit <alexbonwit@mac.com>
2019-10-27 02:13:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,3 +18,22 @@ def using_shift(array)
   array.shift
 end
 
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1 + array2
+end
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+
